74b55265f434fbfd1f390a385a66125c822f7a10
hajayohan-hub <hajayohan@gmail.com>
2026-08-01 12:10:39 +0300

    fix: suppression de skipWaiting() dans install pour laisser le bandeau visible

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ const STATIC_ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
-    .then(() => self.skipWaiting())
+
   );
 });
 
